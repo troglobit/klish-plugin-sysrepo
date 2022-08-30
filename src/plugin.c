c24be04cfb8ced11df8f95bb90ce22515ed9caa5
Serj Kalichev <serj.kalichev@gmail.com>
2022-08-30 16:26:07 +0300

    Plugin connects to Sysrepo

--- src/plugin.c
+++ src/plugin.c
@@ -10,6 +10,9 @@
 #include <klish/kplugin.h>
 #include <klish/kcontext.h>
 
+#include <sysrepo.h>
+#include <sysrepo/xpath.h>
+
 #include "private.h"
 
 
@@ -21,10 +24,27 @@ int kplugin_sysrepo_init(kcontext_t *context)
 {
 	kplugin_t *plugin = NULL;
 	ksym_t *sym = NULL;
+	int err = SR_ERR_OK;
+	sr_conn_ctx_t *conn = NULL;
+	sr_session_ctx_t *sess = NULL;
 
 	assert(context);
 	plugin = kcontext_plugin(context);
 	assert(plugin);
+
+	err = sr_connect(SR_CONN_DEFAULT, &conn);
+	if (err) {
+		fprintf(stderr, "Can't connect to Sysrepo\n");
+		return -1;;
+	}
+	err = sr_session_start(conn, SR_DS_RUNNING, &sess);
+	if (err) {
+		fprintf(stderr, "Can't start Sysrepo session\n");
+		sr_disconnect(conn);
+		return -1;
+	}
+
+	kplugin_set_udata(plugin, sess);
 
 /*
 	// Misc
@@ -62,7 +82,16 @@ int kplugin_sysrepo_init(kcontext_t *context)
 
 int kplugin_sysrepo_fini(kcontext_t *context)
 {
-	context = context; // Happy compiler
+	kplugin_t *plugin = NULL;
+	sr_session_ctx_t *sess = NULL;
+
+	if (!context)
+		return -1;
+
+	plugin = kcontext_plugin(context);
+	sess = (sr_session_ctx_t *)kplugin_udata(plugin);
+
+	sr_disconnect(sr_session_get_connection(sess));
 
 	return 0;
 }
